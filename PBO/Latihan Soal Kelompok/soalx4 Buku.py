5d99d516988ce6956b284d08f52188f0a54ee8de
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2024-02-26 15:36:32 +0700

    Create soalx4 Buku.py

---
+++ PBO/Latihan Soal Kelompok/soalx4 Buku.py
@@ -0,0 +1,6 @@
+class Book:
+  def __init__(self, title, author, publication year, publisher, isbn):
+    self.title = title
+    self.autor = author
+    self.publication year = publication_year
+    sel
