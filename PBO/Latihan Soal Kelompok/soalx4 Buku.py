2d8146ca6956303140211861d9dd21c915509877
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2024-02-26 15:45:16 +0700

    Update soalx4 Buku.py

--- PBO/Latihan Soal Kelompok/soalx4 Buku.py
+++ PBO/Latihan Soal Kelompok/soalx4 Buku.py
@@ -2,5 +2,23 @@ class Book:
   def __init__(self, title, author, publication year, publisher, isbn):
     self.title = title
     self.autor = author
-    self.publication year = publication_year
-    sel
+    self.publication_year = publication_year
+    self.publisher = publisher
+    self.isbn = isbn
+    self.available = True
+  def borrow_book(self):
+    if self.available:
+      self.available = False
+      print(f"{self.title} sudah dipinjam")
+    else:
+      print(f"{self.title} buku tidak ada")
+
+  def return_book(self):
+    if not self.available:
+      self.available = True
+      print(f"{self.title} sudah dikembalikan")
+    else:
+      print(f"{self.title} sudah ada")
+    def displai_book(self):
+      print(f"title: {self.title}")
+      
