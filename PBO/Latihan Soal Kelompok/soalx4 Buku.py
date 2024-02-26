3564b5ee27175651befb77d68be91c9c9828c063
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2024-02-26 15:53:40 +0700

    Update soalx4 Buku.py

--- PBO/Latihan Soal Kelompok/soalx4 Buku.py
+++ PBO/Latihan Soal Kelompok/soalx4 Buku.py
@@ -21,4 +21,27 @@ class Book:
       print(f"{self.title} sudah ada")
     def displai_book(self):
       print(f"title: {self.title}")
-      
+      print(f"author: {self.author}")
+      print(f"publication year: {self.publication_year}")
+      print(f"publisher: {self.publisher}")
+      print(f"ISBN: {self.isbn}")
+      print(f"Available: {self.available}\n")  
+
+class Library:
+  def __init__(self, books=None):
+    if books is Noone:
+      self.books = []
+    else:
+      self.books = books
+
+  def add_book(self,book):
+    self.books.append(book)
+
+  def remove_book(self, book):
+    for book in self.books:
+      book.display_book()
+
+book1 = Book("Harry Potter", "J.K Rowling", 2000, "Cukurukuk", "32230374830")
+library = Library()
+library.add_book(book1)
+    
