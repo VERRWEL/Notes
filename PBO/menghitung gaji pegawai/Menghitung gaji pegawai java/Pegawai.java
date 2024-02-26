440884e56ada9b4ff0d0306488eadf15ce522a7e
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2024-02-26 12:05:23 +0700

    Add files via upload

---
+++ PBO/menghitung gaji pegawai/Menghitung gaji pegawai java/Pegawai.java
@@ -0,0 +1,39 @@
+package pegawai;
+
+
+public class Pegawai {
+    private int nik;
+    private String name;
+    private double gaji;
+    
+    public Pegawai(int nik, String nama, double gaji){
+        this.nik = nik;
+        this.name = nama;
+        this.gaji = gaji;
+    }
+    
+    public int getNik(){
+        return nik;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public double getGaji() {
+        return gaji;
+    }
+
+
+    public double uang_Makan() {
+    return gaji * 0.1;
+    }
+    
+    public double transport() {
+        return gaji * 0.1;
+    }
+    
+    public double totalGaji() {
+        return gaji + uang_Makan() + transport();
+    }
+}
