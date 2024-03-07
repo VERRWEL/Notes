8d37510f66f58129eedb95be48b43d6f73119b32
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2024-03-07 08:55:49 +0700

    Create Linked LIst.py
    ini adalah codingan yang mencontohkan penerapan linked list dalam ular

---
+++ stuctdat/Linked LIst.py
@@ -0,0 +1,30 @@
+#membuat class Node untuk menyimpan nilai dan alamat dari elemen berikutnnya
+class Node:
+    def __init__(self):
+        self.data = input("Data: ")
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def add1st(self):           #Fungsi menambahkan elemen di linkedlist
+        simpul = Node()         #var simpul = fungsi Node() yang membuattnya memiliki constructor dari class Node()
+        if self.head is None:
+            self.head = simpul
+            return
+        simpul.next = self.head
+        self.head = simpul
+    
+    def print(self):            #fungsi unruk menampilkan isi dari linked list
+        temp = self.head
+        while temp:
+            print(temp.data, end="-->")
+            temp = temp.next
+        print('Name\n')
+
+#mengakses class single linked list dan menambahkan elemen ke dalam list
+sll = LinkedList()
+sll.add1st()
+sll.add1st()
+sll.print()
