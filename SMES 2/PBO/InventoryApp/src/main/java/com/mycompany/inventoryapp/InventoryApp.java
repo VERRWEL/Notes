90d492b54ffee0c6e15970f9742a5e0d772bdeed
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2024-05-06 11:06:31 +0700

    Add files via upload

---
+++ SMES 2/PBO/InventoryApp/src/main/java/com/mycompany/inventoryapp/InventoryApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventoryapp;
+
+/**
+ *
+ * @author LAB1-01
+ */
+public class InventoryApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
