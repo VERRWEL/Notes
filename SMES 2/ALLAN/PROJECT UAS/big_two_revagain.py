beb3592d2ce72a7ecd54b7abc5927d1d2285cc83
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2024-05-11 18:30:20 +0700

    modified:   SMES 2/ALLAN/PROJECT UAS/big_two_revagain.py

--- SMES 2/ALLAN/PROJECT UAS/big_two_revagain.py
+++ SMES 2/ALLAN/PROJECT UAS/big_two_revagain.py
@@ -20,24 +20,24 @@ def ShowDealerCards(): #method untuk menunjukkan semua kartu yang ada
         print(f"{i} : {cards_list[i]}")
 
 def GiveCard(): #method tuntuk membagikan kartu pada masing-masing pemain seacra acak
-    temp = random.choice(list(players))
-    if len(list(temp)) == 13:
+    choose_player = random.choice(list(players))
+    if len(list(choose_player)) == 13:
         GiveCard()
     else:
-        ran = random.randint(0, len(cards_list))
+        choose_card = random.randint(0, len(cards_list))
         try:
-            temp[list(cards_list)[ran]] = cards_list[list(cards_list)[ran]]
-            cards_list.pop(list(cards_list)[ran])
+            choose_player[list(cards_list)[choose_card]] = cards_list[list(cards_list)[choose_card]]
+            cards_list.pop(list(cards_list)[choose_card])
         except:
             print()
 
 def ShowAllPlayerCards():
-    print(f"{Fore.GREEN}PLAYER 1 : {list(p1)} \n{Fore.BLUE}PLAYER 2 : {list(p2)} \n{Fore.RED}PLAYER 3 : {list(p3)} \n{Fore.YELLOW}PLAYER 4 : {list(p4)}{res}")
+    print(f"{Fore.GREEN}PLAYER 1 : {list(p1)} \n{Fore.BLUE}PLAYER 2 : {list(p2)} \n{Fore.MAGENTA}PLAYER 3 : {list(p3)} \n{Fore.YELLOW}PLAYER 4 : {list(p4)}{res}")
     print(len(p1)," : player one cards total")
     print(len(p2)," : player two cards total")
     print(len(p3)," : player three cards total")
     print(len(p4)," : player four cards total")
-    print(len(cards_list), ": cards left to give")
+    print(len(cards_list)," : cards left to give")
 
 def Play():
     pass
@@ -50,7 +50,6 @@ p4 = {}
 cl = {} #card-list
 players = [p1, p2, p3, p4] #list yang menyimpan masing-masing dictionary p1, p2, p3, p4
 
-CardGenerate(cl)
 cards_list = CardGenerate(cl)
 
 while len(cards_list) != 0:
