4fc64411da9467b94cf86f3f4ae16490f90f1a3c
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2024-05-12 23:06:49 +0700

    modified:   SMES 2/ALLAN/PROJECT UAS/big_two_revagain.py 	modified:   other/Lilist.py

--- SMES 2/ALLAN/PROJECT UAS/big_two_revagain.py
+++ SMES 2/ALLAN/PROJECT UAS/big_two_revagain.py
@@ -1,18 +1,21 @@
 import random
 import time
-from colorama import Fore, Back, Style
-red = Fore.RED
-Bred = Back.RED + Fore.BLACK
-Bgrin = Back.GREEN + Fore.BLACK
-Black = Fore.BLACK
-res = Style.RESET_ALL
+try:
+    from colorama import Fore, Back, Style
+    red = Fore.RED
+    Bred = Back.RED + Fore.BLACK
+    Bgrin = Back.GREEN + Fore.BLACK
+    Black = Fore.BLACK
+    res = Style.RESET_ALL
+except:
+    print("colorma not included")
 
 def CardGenerate(cl): #fungsi menghasilkan semua 52 kartu main secara otomatis
-    #style = str(input("use Unicode or Alphabet for symbol? 1/2 : "))
-    #if style == "1": # memilih penggunaan output yang akan digunakan untuk simbol kartu
-    #    default_symbols = ['♦️', '♣️', '♥️', '♠️'] #list simbol limbol kartu (logo/Unicode)
-    #elif style == "2":
-    default_symbols = ['D', 'C', 'H', 'S'] #list simbol limbol kartu (alfabet)
+    style = str(input("use Unicode or Alphabet for symbol? 1/2 : "))
+    if style == "1": # memilih penggunaan output yang akan digunakan untuk simbol kartu
+        default_symbols = ['♦️', '♣️', '♥️', '♠️'] #list simbol limbol kartu (logo/Unicode)
+    elif style == "2":
+        default_symbols = ['D', 'C', 'H', 'S'] #list simbol limbol kartu (alfabet)
     l = 1
     for i in default_symbols:   
         for j in range(1, 14):
@@ -36,7 +39,7 @@ def GiveCard(): #method tuntuk membagikan kartu pada masing-masing pemain seacra
         except:
             print()
 
-def ShowAllPlayerCards():
+def ShowAllPlayerCards(): #method untuk menunjukkan semua kartu yang ada pada masing masing pemain
     print(f"{Fore.GREEN}PLAYER 1 : {list(p1)} \n{Fore.BLUE}PLAYER 2 : {list(p2)} \n{Fore.MAGENTA}PLAYER 3 : {list(p3)} \n{Fore.YELLOW}PLAYER 4 : {list(p4)}{res}")
     """
     print(len(p1)," : player one cards total")
@@ -46,13 +49,12 @@ def ShowAllPlayerCards():
     print(len(cards_list)," : cards left on the dealer")
     """
     
-def Play():
+def Play(): #method untuk memainkan game▮▮▮▬▬▬▬▬▬▬
     print()
     time.sleep(0.3)
     print(Back.WHITE + Fore.BLACK + "Game initiating..." + res)
     time.sleep(0.3)
     print("\nYou are player number one.")
-    #srtd_hand = sorted(p1.items(), key=lambda x: x[1])
     print("Here is your hand:")
     for i in p1:
         print(Fore.GREEN + f"{i}" + res)
@@ -62,11 +64,12 @@ def Play():
     for turn in players:
         print()
         print(f"Player {players.index(turn)+1}")
-        #print(turn)
         for check in turn:
-            if (turn[check] == 1) or (turn[check] == 14) or (turn[check] == 27) or (turn[check] == 40):
-                print(check, turn[check])
-
+            if (turn[check] == 1) or (turn[check] == 14) or (turn[check] == 27):
+                print(check)
+            elif turn[check] == 40:
+                temp = turn[check]
+                print(red + f"{check}" ,res)
 
 ### -MAIN PROGRAM- ###
 p1 = {}
@@ -74,12 +77,12 @@ p2 = {}
 p3 = {}
 p4 = {}
 cl = {} #card-list
-cards_on_the_table = {}
+cards_on_the_table = {} #kartu yang telah dibuang dan sudah tidak dipegang oleh para pemain maupun dealer
 players = [p1, p2, p3, p4] #list yang menyimpan masing-masing dictionary p1, p2, p3, p4
 
 cards_list = CardGenerate(cl)
 
-ShowDealerCards()
+#ShowDealerCards()
 while len(cards_list) != 0:
     #ShowDealerCards()
     #print()
--- other/Lilist.py
+++ other/Lilist.py
@@ -1,23 +1,5 @@
-
-
-mhs = []
-
-while True:
-  data = {}
-  data["nama"] = input("Masukkan Nama\t: ")
-  data["nim"]  = input("Masukkan NIM\t: ")
-  data["ipk"]  = float(input("Masukkan IPK\t: "))
-
-  mhs.append(data)
-
-  print()
-  konfirmasi = input("Masukkan Lagi (Y/N)? ").upper()
-  print()
-  if (konfirmasi == "N"):
-    break
-
-for i in range(len(mhs)):
-  print("Nama :", mhs[i]["nama"], "\t| Alamat:", (mhs[i]["nama"]))
-  print("NIM  :", mhs[i]["nim"] , "\t| Alamat:", (mhs[i]["ipk"]))
-  print("IPK  :", mhs[i]["ipk"] , "\t| Alamat:", (mhs[i]["ipk"]))
-  print()
+import time
+for i in range(1,3):
+  
+  time.sleep(1)
+  print(f"daada ", end=" ")
