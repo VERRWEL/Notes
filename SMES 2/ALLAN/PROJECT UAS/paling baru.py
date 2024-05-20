b04eda3d63a81b8b28e39f1b79a1cf1c67ba2e2a
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2024-05-21 03:07:20 +0700

    modified:   SMES 2/ALLAN/PROJECT UAS/paling baru.py

--- SMES 2/ALLAN/PROJECT UAS/paling baru.py
+++ SMES 2/ALLAN/PROJECT UAS/paling baru.py
@@ -148,7 +148,7 @@ def RecursivePlay(turns, win, skip): #fungsi rekursif untuk memainkan game secar
     else:
         print(f"\nskiped {skip} times")
         if skip == 3:
-            cards_on_the_table[list(cards_on_the_table.values)[-1]] == 0
+            cards_on_the_table[list(cards_on_the_table.keys())[-1]] = 0
             print(cards_on_the_table[list(cards_on_the_table.values)[-1]], "became free")
             skip = 0 
         
