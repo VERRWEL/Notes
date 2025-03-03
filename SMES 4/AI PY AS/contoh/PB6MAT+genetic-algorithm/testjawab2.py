51ff06ce8c1761b613a7948e15bc58f9e6d67edb
VERRWEL <72255440+VERRWEL@users.noreply.github.com>
2025-03-03 10:40:19 +0700

    Changes to be committed: 	modified:   SMES 4/AI PY AS/contoh/PB6MAT+genetic-algorithm/tempCodeRunnerFile.py 	modified:   SMES 4/AI PY AS/contoh/PB6MAT+genetic-algorithm/testjawab2.py

--- SMES 4/AI PY AS/contoh/PB6MAT+genetic-algorithm/testjawab2.py
+++ SMES 4/AI PY AS/contoh/PB6MAT+genetic-algorithm/testjawab2.py
@@ -11,8 +11,7 @@ def MakeIndividual(): # membuat satu individu
         single_rand_gene.append(random.choice(GENES))
     return single_rand_gene
 
-
-def JoinChar(unjoined_indiv): # mengkombinasi masing-masing char dalam variabel 'TARGET' menjadi string
+def JoinChar(unjoined_indiv): # mengkombinasi masing-masing char menjadi string
     joined_indiv = ''.join(unjoined_indiv)
     return joined_indiv
 
@@ -20,12 +19,11 @@ def SeparateChar(joined_indiv): # memisahkan masing-masing char menjadi list
     unjoined_indiv = list(joined_indiv)
     return unjoined_indiv
 
-def FillPopulation(): #memenuhi populasi
+def FillPopulation(): #membuat individu sebanyak POPULATION_SIZE
     individuals = {}
     for i in range(POPULATION_SIZE):
         individuals[JoinChar(MakeIndividual())] = 0
     return individuals
-FP = FillPopulation()
 
 def CalcFitness(initial_population): #mengkalkulasikan poin fitness masing masing individu
 
@@ -36,27 +34,29 @@ def CalcFitness(initial_population): #mengkalkulasikan poin fitness masing masin
         fitness = 0
         iteration = 0
         sep_key = SeparateChar(qi)
+
         for char in sep_key:
             if char == TARGET[iteration]:
                 fitness += 1
             else:
                 fitness = fitness
-
             iteration += 1
+            
         calculated_population[qi] = fitness
-        
-    return calculated_population
 
-def SortPopulation(calculated_population): # mengurutkan masing masing individu secara descending berdasarkan poin fitness
-    fitness_sorted_population = sorted(calculated_population.items(), key=lambda x: x[1], reverse=True)
+    return calculated_population
+CFFP = CalcFitness(FillPopulation())
+
+def SortPopulation(unsorted_calc_population): # mengurutkan masing masing individu secara descending berdasarkan poin fitness
+    fitness_sorted_population = sorted(unsorted_calc_population.items(), key=lambda x: x[1], reverse=True)
     return dict(fitness_sorted_population)
 
 
 print(f"Target = {TARGET}")
 print("\nUnsorted population : ")
-for a,b in CalcFitness(FP).items():
+for a,b in CFFP.items():
     print(a,b)
 
 print("\nSorted population : ")
-for a,b in SortPopulation(CalcFitness(FP)).items():
+for a,b in SortPopulation(CFFP).items():
     print(a,b)
